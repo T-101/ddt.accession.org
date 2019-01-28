24af041a2a154fe2d5b1f6e3394674c2052c1d09
Antti Rummukainen <t-101@t-101.org>
2019-01-28 19:06:48 +0200

    proper semantic checkbox for hiding name/handle

--- ddt/forms.py
+++ ddt/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, NumberInput
+from django.forms import ModelForm, NumberInput, TextInput, Textarea
 from snowpenguin.django.recaptcha2.fields import ReCaptchaField
 from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
 
@@ -12,7 +12,8 @@ class RegistrationForm(ModelForm):
         model = Registration
         fields = '__all__'
         widgets = {
-            'amount': NumberInput(attrs={'min': 1, 'max': 10})
+            'handle': TextInput(attrs={'placeholder': 'Name or Handle'}),
+            'amount': NumberInput(attrs={'min': 1, 'max': 10, 'placeholder': 'Number between 1-10'})
         }
 
 
@@ -22,3 +23,7 @@ class WallForm(ModelForm):
     class Meta:
         model = Wall
         fields = '__all__'
+        widgets = {
+            'handle': TextInput(attrs={'placeholder': 'Name or Handle'}),
+            'text': Textarea(attrs={'placeholder': 'Free text'})
+        }
